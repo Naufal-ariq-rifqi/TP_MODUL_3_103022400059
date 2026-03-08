bdabfda5742ab3a8be7470870af57f71d6826103
Naufal <sagirisamaa@gmail.com>
2026-03-08 15:06:04 +0700

    Menambahkan implementasi kalkulator

--- TP_MODUL3_103022400059/Form1.Designer.cs
+++ TP_MODUL3_103022400059/Form1.Designer.cs
@@ -28,12 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            panel1 = new Panel();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            button12 = new Button();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F);
+            label1.Location = new Point(133, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 46);
+            label1.TabIndex = 0;
+            label1.Text = "0";
+            label1.TextAlign = ContentAlignment.MiddleRight;
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(button12);
+            panel1.Location = new Point(243, 34);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(305, 367);
+            panel1.TabIndex = 1;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(3, 154);
+            button11.Name = "button11";
+            button11.Size = new Size(94, 29);
+            button11.TabIndex = 11;
+            button11.Text = "1";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(103, 154);
+            button10.Name = "button10";
+            button10.Size = new Size(94, 29);
+            button10.TabIndex = 10;
+            button10.Text = "2";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += button10_Click;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(203, 154);
+            button9.Name = "button9";
+            button9.Size = new Size(94, 29);
+            button9.TabIndex = 9;
+            button9.Text = "3";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += button9_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(3, 189);
+            button8.Name = "button8";
+            button8.Size = new Size(94, 29);
+            button8.TabIndex = 8;
+            button8.Text = "4";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(103, 189);
+            button7.Name = "button7";
+            button7.Size = new Size(94, 29);
+            button7.TabIndex = 7;
+            button7.Text = "5";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(203, 189);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 6;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(3, 224);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 5;
+            button5.Text = "7";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(103, 224);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 4;
+            button4.Text = "8";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(203, 224);
+            button3.Name = "button3";
+            button3.Size = new Size(94, 29);
+            button3.TabIndex = 3;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(3, 267);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 2;
+            button2.Text = "+";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(203, 267);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "=";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(103, 267);
+            button12.Name = "button12";
+            button12.Size = new Size(94, 29);
+            button12.TabIndex = 12;
+            button12.Text = "0";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += button12_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form1";
             Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private Button button12;
     }
 }
--- TP_MODUL3_103022400059/Form1.cs
+++ TP_MODUL3_103022400059/Form1.cs
@@ -2,9 +2,171 @@ namespace TP_MODUL3_103022400059
 {
     public partial class Form1 : Form
     {
+        double total = 0;       
+        bool operasiPlus = false; 
+        bool isNewEntry = true;  
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "0")
+            {
+                label1.Text = "0";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "0";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "3")
+            {
+                label1.Text = "3";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "3";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "6")
+            {
+                label1.Text = "6";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "6";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "7")
+            {
+                label1.Text = "7";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "7";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (operasiPlus)
+            {
+                total += Convert.ToDouble(label1.Text);
+                label1.Text = total.ToString();
+                total = 0;
+                operasiPlus = false;
+                isNewEntry = true;
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "1")
+            {
+                label1.Text = "1";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "1";
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "2")
+            {
+                label1.Text = "2";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "2";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "4")
+            {
+                label1.Text = "4";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "4";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "5")
+            {
+                label1.Text = "5";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "5";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "8")
+            {
+                label1.Text = "8";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "8";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (isNewEntry || label1.Text == "9")
+            {
+                label1.Text = "9";
+                isNewEntry = false;
+            }
+            else
+            {
+                label1.Text += "9";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            total += Convert.ToDouble(label1.Text);
+            isNewEntry = true; 
+            operasiPlus = true;
         }
     }
 }
